255b54387bbf243323faf4bf3b61ddbaedb43a1a
panagia-voitheia <pxariton@gmail.com>
2026-07-10 14:13:38 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,5 @@
+importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
+
 self.addEventListener('install',e=>self.skipWaiting());
 self.addEventListener('activate',e=>self.clients.claim());
 self.addEventListener('fetch',event=>{
